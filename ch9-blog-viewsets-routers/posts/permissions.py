590f97722d62fbef84a5810c25229c7ca2eed17a
Will Vincent <will@wsvincent.com>
2022-03-22 11:53:47 -0400

    update permissions

--- ch9-blog-viewsets-routers/posts/permissions.py
+++ ch9-blog-viewsets-routers/posts/permissions.py
@@ -9,5 +9,9 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request so we'll always
+        # allow GET, HEAD, or OPTIONS requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
         # Write permissions are only allowed to the author of a post
         return obj.author == request.user
