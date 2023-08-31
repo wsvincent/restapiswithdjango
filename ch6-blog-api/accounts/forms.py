f46c92bfb2295b6f83320f68b6496d91583154f6
jaypee15 <okoyejohnpaul15@gmail.com>
2023-08-31 22:46:36 +0100

    class Meta subclassing UserCreationForm was removed

--- ch6-blog-api/accounts/forms.py
+++ ch6-blog-api/accounts/forms.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+    class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("name",)
 
